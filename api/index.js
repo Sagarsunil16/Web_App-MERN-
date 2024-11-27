4095b0c1ff7b9bb381936173029d35f0cf105760
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2024-11-27 13:27:03 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-dotenv.config()
-mongoose.connect(process.env.MONGO).then(()=>console.log("Connected to MongoDb")).catch((error)=>console.log(error))
-const app = express()
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("Connected to MongoDb"))
+  .catch((error) => console.log(error));
+const app = express();
 
-app.listen(3000,()=>{
-    console.log("Server is listening on Port:3000")
-})
+app.listen(3000, () => {
+  console.log("Server is listening on Port:3000");
+});
