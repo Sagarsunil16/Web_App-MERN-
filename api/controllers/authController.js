ce07ad587e4d60ecab25f60979f8d9539cfbd438
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2024-11-27 18:39:44 +0530

    create sign in api route

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/userModel.js"
 import bcryptjs from 'bcryptjs'
 import { errorhandler } from "../utils/error.js"
-
+import jwt from 'jsonwebtoken'
 const securePaswword = (password)=>{
     return bcryptjs.hashSync(password,10)
 }
@@ -14,5 +14,26 @@ export const signup = async(req,res,next)=>{
         res.status(201).json({message:"User Created Successfully"})
     } catch (error) {
       next(error)
+    }
+}
+
+export const signin = async(req,res,next)=>{
+    try {
+        const {email,password} = req.body
+        const validUser = await User.findOne({email:email})
+        if(!validUser){
+            return errorhandler(404,"User not found")
+        }
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword){
+            return errorhandler(401,"Wrong Credentials")
+        }
+        const token = jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const{password:hashedPassword,...rest} = validUser._doc
+        console.log(hashedPassword)
+        const expiryDate = new Date(Date.now()+3600000)
+        res.cookie('access_token',token,{httpOnly:true,expires:expiryDate}).status(200).json(rest)
+    } catch (error) {
+        next(error)
     }
 }
--- client/src/Pages/SignIn.jsx
+++ client/src/Pages/SignIn.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const SignIn = () => {
+  const [formData,setFormData] = useState({})
+  const handleChange = (e)=>{
+    setFormData({...formData,[e.target.id]:e.target.value})
+  }
+  console.log(formData)
   return (
-    <div>
-      Sing In
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+      <form className='flex flex-col gap-4' >
+        <input onChange={handleChange} type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' />
+        <input onChange={handleChange} type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
+        <button className='bg-red-700 p-3 rounded-lg hover:opacity-90'>Sign In</button>
+      </form>
     </div>
   )
 }
