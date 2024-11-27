7a6d5d994e0f80491139dee393b500b8dcc0f5d6
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2024-11-27 15:18:08 +0530

    complete sign up page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,8 +13,8 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}></Route>
         <Route path="/about" element={<About/>}></Route>
-        <Route path="/sign-in" element={<SignIn/>}></Route>
-        <Route path="/sing-up" element={<SignUp/>}></Route>
+        <Route path="/signin" element={<SignIn/>}></Route>
+        <Route path="/signup" element={<SignUp/>}></Route>
         <Route path="/profile" element={<Profile/>}></Route>
       </Routes>
     </BrowserRouter>
--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const SignUp = () => {
   return (
-    <div>
-      Sing Up
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4'>
+        <input type="text" placeholder='Username' id='username' className='bg-slate-100 p-3 rounded-lg' />
+        <input type="email" placeholder='Email' id='email' className='bg-slate-100 p-3 rounded-lg' />
+        <input type="password" placeholder='Password' id='password' className='bg-slate-100 p-3 rounded-lg' />
+        <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-60'>Sign up</button>
+        <button className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-60'>Continue With Google</button>
+      </form>
+      <div className='flex gap-1 mt-2'>
+        <p>Have an account?</p>
+        <Link to={'/signin'}><span className='text-blue-500'>Sign in</span></Link>
+      </div>
     </div>
   )
 }
